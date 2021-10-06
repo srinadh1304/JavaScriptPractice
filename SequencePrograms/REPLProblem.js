354a01387dd0d412dca130e75621bae681a5caf6
srinadh1304 <srinadh1304@gmail.com>
2021-10-06 07:08:08 +0530

    MODIFIED: using random function to get number between 1 and 16

--- SequencePrograms/REPLProblem.js
+++ SequencePrograms/REPLProblem.js
@@ -1,2 +1,4 @@
 let singleDigitNumber = Math.floor(Math.random() * 10);
 console.log("single digit number generated : "+ singleDigitNumber);
+let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
+console.log("value after rolling a die: "+ diceNumber);
