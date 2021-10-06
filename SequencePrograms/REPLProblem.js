f4ec4f50ca0131b9ccb2483b8312efb8b6de1bf5
srinadh1304 <srinadh1304@gmail.com>
2021-10-06 07:10:32 +0530

    MODIFIED: adding two random dice numbers

--- SequencePrograms/REPLProblem.js
+++ SequencePrograms/REPLProblem.js
@@ -1,4 +1,7 @@
 let singleDigitNumber = Math.floor(Math.random() * 10);
 console.log("single digit number generated : "+ singleDigitNumber);
 let diceNumber = (Math.floor(Math.random() * 10) % 6) + 1;
-console.log("value after rolling a die: "+ diceNumber);
+console.log("value after rolling a die first time: "+ diceNumber);
+
+diceNumber += (Math.floor(Math.random() * 10) % 6) + 1;
+console.log("value after rolling twice: "+ diceNumber);
