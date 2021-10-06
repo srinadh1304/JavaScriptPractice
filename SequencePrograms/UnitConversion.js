1f8ea08b6372ed0469217570396c593bc813aa56
srinadh1304 <srinadh1304@gmail.com>
2021-10-06 07:16:06 +0530

    ADDED: propgram to convert one unit to other

---
+++ SequencePrograms/UnitConversion.js
@@ -0,0 +1,9 @@
+let inch = Math.floor(Math.random() * 100);
+let feet = inch / 12;
+
+console.log("Given inch: "+ inch+" converted to feet: "+ feet);
+let rectangularArea = 60 * 40;
+let totalArea = rectangularArea * 25;
+
+let areaInAcres = totalArea / 43560;
+console.log("The area in square feet: "+ totalArea+"  in acres: "+ areaInAcres);
