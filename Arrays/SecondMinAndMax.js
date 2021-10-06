006a905abf3dc831054f4d500e8c0f653328e1c7
srinadh1304 <srinadh1304@gmail.com>
2021-10-06 20:12:35 +0530

    MODIFIED: storing numbers in an array

--- Arrays/SecondMinAndMax.js
+++ Arrays/SecondMinAndMax.js
@@ -1,5 +1,7 @@
+let randomNumberArray = new Array();
 for(let index=0;index<10;index++)
 {
     let number=Math.floor(Math.random()*900)+100;
+    randomNumberArray.push(number);
     console.log(number);
 }
