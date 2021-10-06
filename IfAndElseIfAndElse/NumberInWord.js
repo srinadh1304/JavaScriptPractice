5cbd267ee25b457a97d97fe23a375d84977c52c6
srinadh1304 <srinadh1304@gmail.com>
2021-10-06 07:49:41 +0530

    ADDED: program to write number in word

---
+++ IfAndElseIfAndElse/NumberInWord.js
@@ -0,0 +1,24 @@
+let digit = Math.floor(Math.random() * 10);
+
+console.log("Number generated: "+ digit);
+
+if(digit == 0) 
+    console.log("ZERO");
+else if(digit == 1)
+    console.log("ONE");
+else if(digit == 2)
+    console.log("TWO");
+else if(digit == 3)
+    console.log("THREE");
+else if(digit == 4)
+    console.log("FOUR");
+else if(digit == 5)
+    console.log("FIVE");
+else if(digit == 6)
+    console.log("SIX");
+else if(digit == 7)
+    console.log("SEVEN");
+else if(digit == 8)
+    console.log("EIGHT");
+else
+    console.log("NINE");
